7b3b9c0b696e1255af82f314dbcf7959af2fb136
Adarshrai0 <adarshrai3011@gmail.com>
2022-04-07 08:48:59 +0530

    styled integrated in hands

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-
-
-const secondHand = document.querySelector('.second-hand');
-const minsHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
+const secondHand = document.querySelector(".second-hand");
+const minsHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 function setDate() {
   const now = new Date();
 
   const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90 ;
+  const secondsDegrees = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
+  secondHand.style.height = "4px";
+  
   const mins = now.getMinutes();
-  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  const minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6 + 90;
   minsHand.style.transform = `rotate(${minsDegrees}deg)`;
-
+  minsHand.style.background = "red";
 
   const hour = now.getHours();
-  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  const hourDegrees = (hour / 12) * 360 + (mins / 60) * 30 + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  
 }
 
 setInterval(setDate, 1000);
